803ddc991bf27f7a297fb4a5bc5637af7c1ac2f9
novalrnv <akuunoval@gmail.com>
2024-11-21 07:34:18 +0700

    modyfication p2

--- Kafe12.java
+++ Kafe12.java
@@ -1,5 +1,8 @@
+import java.util.Scanner;
+
 public class Kafe12 {
-    public static void Menu (String namapelanggan, boolean ismember) {
+    static Scanner sc = new Scanner(System.in);
+    public static void Menu (String namapelanggan, boolean ismember, String kodepromo) {
         System.out.println("Selamat datang, " +namapelanggan+ "!");
 
         if (ismember) {
@@ -14,10 +17,25 @@ public class Kafe12 {
         System.out.println("5. Roti Bakar - Rp 10,000");
         System.out.println("6. Mie Goreng - Rp 18,000");
         System.out.println("===== MENU RESTO KAFE =====");
+        
         System.out.println("Silahkan pilih menu yang anda inginkan.");
+
+        switch (kodepromo) {
+            case "DISKON50" :
+                System.out.println("Anda mendapatkan diskon 50%");
+                break;
+    
+            case "DISKON30" :
+                System.out.println("Anda mendapatkan diskon 30%");
+                break;
+    
+            default:
+                System.out.println("Kode invalid!");
+                break;
+        }
     }
 
     public static void main(String[] args) {
-        Menu("Andi", true);
+        Menu("Andi", true, "DISKON90");
     }
 }
